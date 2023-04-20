c1306c435ebba0505d9739aa74c5f7108dde8bd2
vishwaspuri <vishwaspuri09@gmail.com>
2023-04-20 19:59:20 +0530

    Tracking user changes in firestore

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Login';
 import Leaderboard from './Leaderboard';
 import LeaderboardV2 from './LeaderboardV2';
+import LeaderboardV3 from './LeaderboardV3';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
           <Route path="/leaderboard/v2/:sortBy" element={<LeaderboardV2 />} />
+          <Route path="/leaderboard/v3/:sortBy" element={<LeaderboardV3 />} />
         </Routes>
       </div>
     </Router>
